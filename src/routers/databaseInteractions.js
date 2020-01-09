3a4a45392ad7b7612e434db1c49497677a78114c
esszed <56234798+esszed@users.noreply.github.com>
2020-01-09 18:41:48 +0100

    small fix, deleted extra code

--- src/routers/databaseInteractions.js
+++ src/routers/databaseInteractions.js
@@ -1,163 +1,190 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const User = require('../models/user')
 const Item = require('../models/item')
 const auth = require('../middlewares/auth')
 const router = new express.Router()
 
 router.post('/additem', auth, (req, res) => {
-  let accessory
-  for (let i = 0; i < req.body.number; i++) {
-    if (req.body.accessory == '') {
-      accessory = 'Žádné'
-    } else {
-      accessory = req.body.accessory
+  try {
+    let accessory
+    for (let i = 0; i < req.body.number; i++) {
+      if (req.body.accessory == '') {
+        accessory = 'Žádné'
+      } else {
+        accessory = req.body.accessory
+      }
+      const item = new Item({
+        _id: new mongoose.Types.ObjectId(),
+        brand: req.body.brand,
+        name: req.body.name,
+        type: req.body.type,
+        accessory: accessory,
+        borrowedBy: '',
+        state: 'K dispozici'
+      })
+      item.save()
     }
-    const item = new Item({
-      _id: new mongoose.Types.ObjectId(),
-      brand: req.body.brand,
-      name: req.body.name,
-      type: req.body.type,
-      accessory: accessory,
-      borrowedBy: '',
-      state: 'K dispozici'
-    })
-    item.save()
+    res.redirect('/crud')
+  } catch (e) {
+    res.status(500).send()
   }
-  res.redirect('/crud')
 })
 
 router.post('/deleteitems/:name/:brand/:amount', auth, (req, res) => {
-  for (let i = 0; i < req.params.amount; i++) {
-    Item.deleteOne(
-      { brand: req.params.brand, name: req.params.name, state: 'K dispozici' },
-      err => {
-        if (err) return handleError(err)
-      }
-    )
+  try {
+    for (let i = 0; i < req.params.amount; i++) {
+      Item.deleteOne(
+        {
+          brand: req.params.brand,
+          name: req.params.name,
+          state: 'K dispozici'
+        },
+        err => {
+          if (err) return handleError(err)
+        }
+      )
+    }
+    res.redirect('/crud')
+  } catch (e) {
+    res.status(500).send()
   }
-  res.redirect('/crud')
 })
 
 router.post('/deleteitem', auth, (req, res) => {
-  Item.deleteOne({ _id: req.body.id }, err => {
-    if (err) return handleError(err)
-  })
-  res.redirect('/uplnezobrazeni')
+  try {
+    Item.deleteOne({ _id: req.body.id }, err => {
+      if (err) return handleError(err)
+    })
+    res.redirect('/uplnezobrazeni')
+  } catch (e) {
+    res.status(500).send()
+  }
 })
 
 router.post('/borrow/:name/:brand/:amount', auth, (req, res) => {
-  Item.find(
-    { brand: req.params.brand, name: req.params.name, state: 'K dispozici' },
-    (err, items) => {
-      for (let i = 0; i < req.params.amount; i++) {
-        items[i].borrowedBy = req.user._id
-        items[i].state = 'Vypůjčené'
-        items[i].borrowDate = new Date().toLocaleDateString('cs-CZ')
-        items[i].endDate = new Date(Date.now() + 12096e5).toLocaleDateString(
-          'cs-CZ'
-        )
-        items[i].save()
-        req.user.borrowed.push(items[i]._id)
+  try {
+    Item.find(
+      { brand: req.params.brand, name: req.params.name, state: 'K dispozici' },
+      (err, items) => {
+        for (let i = 0; i < req.params.amount; i++) {
+          items[i].borrowedBy = req.user._id
+          items[i].state = 'Vypůjčené'
+          items[i].borrowDate = new Date().toLocaleDateString('cs-CZ')
+          items[i].endDate = new Date(Date.now() + 12096e5).toLocaleDateString(
+            'cs-CZ'
+          )
+          items[i].save()
+          req.user.borrowed.push(items[i]._id)
+        }
+        req.user.save()
       }
-      req.user.save()
-    }
-  )
-  res.redirect('/knihovna')
+    )
+    res.redirect('/knihovna')
+  } catch (e) {
+    res.status(500).send()
+  }
 })
 
 router.post('/return/:name/:brand/', auth, (req, res) => {
-  Item.find(
-    {
-      brand: req.params.brand,
-      name: req.params.name,
-      state: 'Vypůjčené',
-      borrowedBy: req.user._id
-    },
-    (err, items) => {
-      items.forEach(item => {
-        item.borrowedBy = ''
-        item.state = 'K dispozici'
-        item.borrowDate = ''
-        item.endDate = ''
-        item.save()
-        req.user.borrowed.splice(req.user.borrowed.indexOf(item._id), 1)
-      })
-      req.user.save()
-      res.redirect('/knihovna')
-    }
-  )
+  try {
+    Item.find(
+      {
+        brand: req.params.brand,
+        name: req.params.name,
+        state: 'Vypůjčené',
+        borrowedBy: req.user._id
+      },
+      (err, items) => {
+        items.forEach(item => {
+          item.borrowedBy = ''
+          item.state = 'K dispozici'
+          item.borrowDate = ''
+          item.endDate = ''
+          item.save()
+          req.user.borrowed.splice(req.user.borrowed.indexOf(item._id), 1)
+        })
+        req.user.save()
+        res.redirect('/knihovna')
+      }
+    )
+  } catch (e) {
+    res.status(500).send()
+  }
 })
 
 router.post('/change', auth, (req, res) => {
-  Item.find(
-    { name: req.body.originalName, brand: req.body.originalBrand },
-    (err, items) => {
-      items.forEach(item => {
-        if (req.body.newBrand !== '') {
-          item.brand = req.body.newBrand
-        }
-        if (req.body.newName !== '') {
-          item.name = req.body.newName
-        }
-        if (req.body.newType !== '') {
-          item.type = req.body.newType
-        }
-        if (req.body.newAccessory !== '') {
-          item.accessory = req.body.newAccessory
-        }
-        item.save()
-      })
+  try {
+    Item.find(
+      { name: req.body.originalName, brand: req.body.originalBrand },
+      (err, items) => {
+        items.forEach(item => {
+          if (req.body.newBrand !== '') {
+            item.brand = req.body.newBrand
+          }
+          if (req.body.newName !== '') {
+            item.name = req.body.newName
+          }
+          if (req.body.newType !== '') {
+            item.type = req.body.newType
+          }
+          if (req.body.newAccessory !== '') {
+            item.accessory = req.body.newAccessory
+          }
+          item.save()
+        })
 
-      if (req.body.newAmount > 0) {
-        if (req.body.newAmount < items.length) {
-          for (let i = 0; i < items.length - req.body.newAmount; i++) {
-            Item.deleteOne(
-              {
-                brand: req.body.originalBrand,
-                name: req.body.originalName
-              },
-              err => {
-                if (err) return handleError(err)
+        if (req.body.newAmount > 0) {
+          if (req.body.newAmount < items.length) {
+            for (let i = 0; i < items.length - req.body.newAmount; i++) {
+              Item.deleteOne(
+                {
+                  brand: req.body.originalBrand,
+                  name: req.body.originalName
+                },
+                err => {
+                  if (err) return handleError(err)
+                }
+              )
+            }
+          } else if (req.body.newAmount > items.length) {
+            for (let i = 0; i < req.body.newAmount - items.length; i++) {
+              const item = new Item({
+                _id: new mongoose.Types.ObjectId(),
+                borrowedBy: '',
+                state: 'K dispozici'
+              })
+
+              if (req.body.newBrand !== '') {
+                item.brand = req.body.newBrand
               }
-            )
-          }
-        } else if (req.body.newAmount > items.length) {
-          for (let i = 0; i < req.body.newAmount - items.length; i++) {
-            const item = new Item({
-              _id: new mongoose.Types.ObjectId(),
-              borrowedBy: '',
-              state: 'K dispozici'
-            })
+              if (req.body.newName !== '') {
+                item.name = req.body.newName
+              }
+              if (req.body.newType !== '') {
+                item.type = req.body.newType
+              }
 
-            if (req.body.newBrand !== '') {
-              item.brand = req.body.newBrand
-            }
-            if (req.body.newName !== '') {
-              item.name = req.body.newName
-            }
-            if (req.body.newType !== '') {
-              item.type = req.body.newType
-            }
+              if (req.body.newAccessory !== '') {
+                item.accessory = req.body.newAccessory
+              }
 
-            if (req.body.newAccessory !== '') {
-              item.accessory = req.body.newAccessory
-            }
+              if (req.body.newBrand == '') {
+                item.brand = req.body.originalBrand
+              }
+              if (req.body.newName == '') {
+                item.name = req.body.originalName
+              }
 
-            if (req.body.newBrand == '') {
-              item.brand = req.body.originalBrand
+              item.save()
             }
-            if (req.body.newName == '') {
-              item.name = req.body.originalName
-            }
-
-            item.save()
           }
         }
+        res.redirect('/crud')
       }
-      res.redirect('/crud')
-    }
-  )
+    )
+  } catch (e) {
+    res.status(500).send()
+  }
 })
 
 module.exports = router
